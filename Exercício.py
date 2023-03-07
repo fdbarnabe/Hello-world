ac2908b696d56c7c702d3b08c45ab0a3a072921a
fdbarnabe <125517866+fdbarnabe@users.noreply.github.com>
2023-03-07 20:12:00 +0000

    Exercicio Print

---
+++ Exercício.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+endereco = input("Digite seu endereco: ")
+profissao = input("Digite sua profissao: ")
+
+print(f"Nome: {nome} - Endereço: {endereco} - Profissão: {profissao}")
+print("Nome: " + nome + " - Endereço: " + endereco + " - Profissão: " + profissao)
+
