cdc95e9727e45fb3ecac83fc564e57e783fe83d3
fdbarnabe <125517866+fdbarnabe@users.noreply.github.com>
2023-03-22 23:00:56 +0000

    Ex12

--- Exercícios/Ex12.py
+++ Exercícios/Ex12.py
@@ -2,14 +2,14 @@ peso = float(input("Peso: "))
 altura = float(input("Altura: "))
 IMC = peso/(altura*altura);
 if IMC <= 18.5:
-    print(f"Abaixo do Peso")
+    print(f"O seu IMC é {IMC} - Abaixo do Peso")
 elif IMC > 18.5 and IMC <= 24.9:
-    print(f"Saudavel")
+    print(f"O seu IMC é {IMC} - Saudavel")
 elif IMC > 24.9 and IMC <= 29.9:
-    print(f"Peso em excesso")
+    print(f"O seu IMC é {IMC} - Peso em excesso")
 elif IMC > 29.9 and IMC <= 34.9:
-    print(f"Obesidade Grau I")
+    print(f"O seu IMC é {IMC} - Obesidade Grau I")
 elif IMC > 34.9 and IMC <= 39.9:
-    print(f"Obesidade Grau II(severa)")
+    print(f"O seu IMC é {IMC} - Obesidade Grau II(severa)")
 else:
-    print(f"Obesidade Grau III(morbida)")
+    print(f"O seu IMC é {IMC} - Obesidade Grau III(morbida)")
