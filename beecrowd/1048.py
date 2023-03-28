3ed8dcfe07ef95ba30d86aac9816e4cf11f1badf
fdbarnabe <125517866+fdbarnabe@users.noreply.github.com>
2023-03-28 20:41:22 +0000

    bee

---
+++ beecrowd/1048.py
@@ -0,0 +1,24 @@
+a = float(input())
+if a >= 0 and a <= 400:
+    b=(a+(a*15/100))
+    c=a*15/100
+    d=15
+elif a >= 400.01 and a <= 800:
+    b = (a+(a*12/100))
+    c=a*12/100
+    d = 12
+elif a >= 800.01 and a <= 1200:
+    b = (a+(a*10/100))
+    c=a*10/100
+    d = 10
+elif a >= 1200.01 and a <= 2000:
+    b =( a+(a*7/100))
+    c=a*7/100
+    d = 7
+elif a >= 2000.01:
+    b = (a+(a*4/100))
+    c=a*4/100
+    d = 4
+print(f"Novo salario: {b:.2f}")
+print(f"Reajuste ganho: {c:.2f}")
+print(f"Em percentual: {d} %")
