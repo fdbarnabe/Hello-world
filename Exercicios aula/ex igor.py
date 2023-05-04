a8d34e3c44badb63bfcdb7932aa03a833631b7a0
Fernando Domnigues <fbarnabe@outlook.com>
2023-05-03 22:26:35 -0300

    ex

--- Exercicios aula/ex igor.py
+++ Exercicios aula/ex igor.py
@@ -1,5 +1,4 @@
 a = str(input(f"Qual o seu nome: ").strip())
-
 nomesbanidos = ["igor", "fernando", "paulo"]
 amin = a.lower().split()
 a0 = amin[0]
@@ -9,12 +8,12 @@ if a0 not in nomesbanidos:
     if b >= 18 and b<=30:
         print(f"Você não esta apto para continuar o cadastro")
     else:
-        c = str(input(f"Qual o seu endereço: ").strip())
+        c = str(input(f"Qual o seu endereço, começando pela rua: ").strip())
         cmin = c.lower().split()
         c0 = cmin[0]
         cm = c.capitalize()
         while c0 != str("rua"):
             c= str(input(f"Endereço invalido, digite novamente: "))
-        print(f"Cadastro Realizado, nome: {am}, idade: {b}anos, endereço: {cm}")
+        print(f"Cadastro Realizado, nome: {am}, idade: {b} anos, endereço: {cm}")
 else:
     print(f"Você não pode continuar com o cadastro")
