82c9530fdd33e6810f88b23288eea18303c9626a
Fernando Domnigues <fbarnabe@outlook.com>
2023-05-10 20:31:31 -0300

    def

---
+++ calculadora/main.py
@@ -0,0 +1,13 @@
+from calculadora import soma, mult, div, sub
+
+resultado_soma = soma(10,256)
+print(resultado_soma)
+
+resultado_sub = sub(10,256)
+print(resultado_sub)
+
+resultado_mult = mult(10,256)
+print(resultado_mult)
+
+resultado_div = div(10,0)
+print(resultado_div)
