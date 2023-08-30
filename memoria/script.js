5bedae6202a9f36384907cfaec72c35aaafdbaf6
Fernando Domnigues <fbarnabe@outlook.com>
2023-08-30 20:56:28 -0300

    colors

--- memoria/script.js
+++ memoria/script.js
@@ -1,8 +1,32 @@
-let box = document.getElementById("box-1");
-box.style.background = "red";
+const colors = ['blue', 'yellow', 'purple', 'pink', 'green', 'darkblue', 'lightblue', 'lightgreen', 'grey', 'black']
 
-let box = document.getElementById("box-2");
-box.style.background = "blue";
+/* for (let i = 1; i <= 3; i++) {
+    let box = document.getElementById("box-" + i);
+    box.style.background = colors[i - 1];
+} */
 
-let box = document.getElementById("box-3");
-box.style.background = "yellow";
+function changeColor(id) {
+    let box = document.getElementById(id);
+    box.style.background = colors[Math.round(Math.random() * 10)];
+}
+
+
+function init() {
+    const quantity = 13;
+    /* let boxes = document.getElementsByClassName("box");
+
+    Object.values(boxes).forEach((item, index) => {
+        console.log(item, index);
+        console.log(item.id);
+        item.onclick = () => changeColor(item.id);
+    }) */
+    let container = document.getElementById("container");
+    let box = document.createElement("div");
+    box.className = "box";
+    box.id = 'box-1';
+    box.innerText = 1;
+    container.appendChild(box);
+
+}
+
+init()
