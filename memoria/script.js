b2a84ea0fc5bc9fc98716cf511f14b2e7ce364fb
Fernando Domnigues <fbarnabe@outlook.com>
2023-08-30 20:28:45 -0300

    a

--- memoria/script.js
+++ memoria/script.js
@@ -1,8 +1,3 @@
 let box = document.getElementById("box-1");
 box.style.background = "red";
 
-let box = document.getElementById("box-2");
-box.style.background = "blue";
-
-let box = document.getElementById("box-3");
-box.style.background = "yellow";
