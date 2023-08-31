615cd88bd02fafc0f74feaf45fd8c897ccd2dfed
Fernando Domnigues <fbarnabe@outlook.com>
2023-08-30 21:08:30 -0300

    java

--- memoria/script.js
+++ memoria/script.js
@@ -1,8 +1,38 @@
-let box1 = document.getElementById("box-1");
-box1.style.background = "red";
+const colors = ['blue', 'yellow', 'purple', 'pink', 'green', 'darkblue', 'lightblue', 'lightgreen', 'grey', 'black']
 
-let box2 = document.getElementById("box-2");
-box2.style.background = "blue";
+/* for (let i = 1; i <= 3; i++) {
+    let box = document.getElementById("box-" + i);
+    box.style.background = colors[i - 1];
+} */
 
-let box3 = document.getElementById("box-3");
-box3.style.background = "yellow"
+function changeColor(id) {
+    let box = document.getElementById(id);
+    box.style.background = colors[Math.round(Math.random() * 10)];
+}
+
+
+function init() {
+    const quantity = 13;
+    /* let boxes = document.getElementsByClassName("box");
+
+    Object.values(boxes).forEach((item, index) => {
+        console.log(item, index);
+        console.log(item.id);
+        item.onclick = () => changeColor(item.id);
+    }) */
+    let container = document.getElementById("container");
+    let box = document.createElement("div");
+    let n=0;
+    while ( n <=13 ){
+        for (let b=1; b<=13; b++){
+        box.className = "box";
+        box.id = 'box-' + b;
+        box.innerText = b;
+        container.appendChild(box);
+    }
+    n++;
+    }
+    
+}
+
+init()
