5b29180f27a8545368515ada9cdc5e109811c360
Fernando Domnigues <fbarnabe@outlook.com>
2023-09-20 20:50:24 -0300

    index

--- tabuleiro/script.js
+++ tabuleiro/script.js
@@ -2,8 +2,8 @@ function virarCarta(cartaId){
     console.log(cartaId);
     let elemento = document.getElementById(cartaId);
     if (elemento.classList.contains('carta-virada')){
-        element.classList.remove('carta-virada');
+        elemento.classList.remove('carta-virada');
     }else{
-        element.classList.add('carta-virada');
+        elemento.classList.add('carta-virada');
     }
 }
